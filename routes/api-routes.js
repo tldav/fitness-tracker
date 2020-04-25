924fb3407c634b632ab2ce56f460601d13008903
Neverender <tldavis09@yahoo.com>
2020-04-24 22:19:22 -0500

    DEFINITELY overthinking it. Only had to find all workouts...the function in api.js did the rest. derp

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,20 +2,9 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
-	// const lastWorkout = req.params.length - 1;
-	// Workout.findOne(lastWorkout)
-	// 	.then((foundWorkout) => {
-	// 		res.json(foundWorkout);
-	// 	})
-	// 	.catch((err) => {
-	// 		res.status(400).json(err);
-	//     });
-
-	Workout.find({})
-		.then((all) => {
-			console.log(all);
-			const lastWorkout = all.length - 1;
-			res.json(lastWorkout);
+	Workout.find()
+		.then((allWorkouts) => {
+			res.json(allWorkouts);
 		})
 		.catch((err) => {
 			res.status(400).json(err);
