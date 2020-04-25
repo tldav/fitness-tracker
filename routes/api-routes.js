2ad999154adafec2cb006d464031f24823a9ddc3
Neverender <tldavis09@yahoo.com>
2020-04-24 21:45:44 -0500

    put request working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,9 +3,9 @@ const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", ({ body }, res) => {
 	Workout.create(body)
-		.then((dbWorkout) => {
-			console.log(dbWorkout);
-			res.json(dbWorkout);
+		.then((newWorkout) => {
+			console.log(newWorkout);
+			res.json(newWorkout);
 		})
 		.catch((err) => {
 			res.status(400).json(err);
@@ -13,7 +13,17 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-	Workout.updateOne({ _id: req.params.id }, {});
+	Workout.updateOne(
+		{ _id: req.params.id },
+		{ $push: { exercises: req.body } }
+	)
+		.then((newExercise) => {
+			console.log(newExercise);
+			res.json(newExercise);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
 });
 
 module.exports = router;
